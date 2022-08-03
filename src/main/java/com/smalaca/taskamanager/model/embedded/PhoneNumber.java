70c5a295f087d56e3f44199033a4b8cfa8ac35fa
Marcin Chrost <mchrost82@gmail.com>
2022-08-03 13:30:29 +0200

    REFACT / TASK - PhoneNumber - introduce all arguments constructor, mark default constructor and setter as deprecated and use lombok to remove boilerplate code

--- src/main/java/com/smalaca/taskamanager/model/embedded/PhoneNumber.java
+++ src/main/java/com/smalaca/taskamanager/model/embedded/PhoneNumber.java
@@ -1,5 +1,9 @@
 package com.smalaca.taskamanager.model.embedded;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
@@ -7,6 +11,9 @@ import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
+@AllArgsConstructor
+@EqualsAndHashCode
+@Getter
 public class PhoneNumber {
     @Column(name = "phone_prefix")
     private String prefix;
@@ -14,46 +21,17 @@ public class PhoneNumber {
     @Column(name = "phone_number")
     private String number;
 
-    public String getPrefix() {
-        return prefix;
-    }
+    @Deprecated
+    public PhoneNumber() {}
 
+    @Deprecated
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }
 
-    public String getNumber() {
-        return number;
-    }
-
+    @Deprecated
     public void setNumber(String number) {
         this.number = number;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        PhoneNumber that = (PhoneNumber) o;
-
-        return new EqualsBuilder()
-                .append(prefix, that.prefix)
-                .append(number, that.number)
-                .isEquals();
-    }
-
-    @Override
-    @SuppressWarnings("MagicNumber")
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(prefix)
-                .append(number)
-                .toHashCode();
-    }
 }
