6dc8032e16b9f5cbab6be21e3bcd827695779ff0
Marcin Chrost <mchrost82@gmail.com>
2022-08-03 23:21:13 +0200

    REFACT / TASK - remove unused code from UserDto

--- src/main/java/com/smalaca/taskamanager/dto/UserDto.java
+++ src/main/java/com/smalaca/taskamanager/dto/UserDto.java
@@ -1,105 +1,24 @@
 package com.smalaca.taskamanager.dto;
 
 import com.smalaca.taskamanager.model.enums.TeamRole;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
 
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@Getter
 public class UserDto {
-    private Long id;
-    private String firstName;
-    private String lastName;
-    private String login;
-    private String password;
-    private String phoneNumber;
-    private String phonePrefix;
-    private String emailAddress;
-    private String teamRole;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    @Deprecated
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getPhonePrefix() {
-        return phonePrefix;
-    }
-
-    @Deprecated
-    public void setPhonePrefix(String phonePrefix) {
-        this.phonePrefix = phonePrefix;
-    }
-
-    public String getEmailAddress() {
-        return emailAddress;
-    }
-
-    public void setEmailAddress(String emailAddress) {
-        this.emailAddress = emailAddress;
-    }
-
-    public String getTeamRole() {
-        return teamRole;
-    }
-
-    public void setTeamRole(String teamRole) {
-        this.teamRole = teamRole;
-    }
+    private final Long id;
+    private final String firstName;
+    private final String lastName;
+    private final String login;
+    private final String password;
+    private final String phoneNumber;
+    private final String phonePrefix;
+    private final String emailAddress;
+    private final String teamRole;
 
     public TeamRole asTeamRole() {
         return TeamRole.valueOf(teamRole);
-    }
-
-    public void setPhoneNumber(String phonePrefix, String phoneNumber) {
-        this.phonePrefix = phonePrefix;
-        this.phoneNumber = phoneNumber;
     }
 
     public static class UserDtoBuilder {
