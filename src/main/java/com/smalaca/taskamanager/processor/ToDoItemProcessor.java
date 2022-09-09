f8f30e303f314e5e7b2a848cb8a626809678aa3a
Marcin Chrost <mchrost82@gmail.com>
2022-09-09 11:07:22 +0200

    REFACT / PATTERN - added rest of ToDoItemState implementations

--- src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
+++ src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
@@ -1,20 +1,16 @@
 package com.smalaca.taskamanager.processor;
 
-import static com.smalaca.taskamanager.model.enums.ToDoItemStatus.DONE;
-
-import com.smalaca.taskamanager.events.StoryApprovedEvent;
-import com.smalaca.taskamanager.events.StoryDoneEvent;
-import com.smalaca.taskamanager.events.TaskApprovedEvent;
-import com.smalaca.taskamanager.events.ToDoItemReleasedEvent;
-import com.smalaca.taskamanager.model.entities.Story;
-import com.smalaca.taskamanager.model.entities.Task;
 import com.smalaca.taskamanager.model.interfaces.ToDoItem;
 import com.smalaca.taskamanager.registry.EventsRegistry;
 import com.smalaca.taskamanager.service.CommunicationService;
 import com.smalaca.taskamanager.service.ProjectBacklogService;
 import com.smalaca.taskamanager.service.SprintBacklogService;
 import com.smalaca.taskamanager.service.StoryService;
+import com.smalaca.taskamanager.state.ToDoItemApprovedState;
 import com.smalaca.taskamanager.state.ToDoItemDefinedState;
+import com.smalaca.taskamanager.state.ToDoItemDoneState;
+import com.smalaca.taskamanager.state.ToDoItemInProgressState;
+import com.smalaca.taskamanager.state.ToDoItemReleasedState;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -43,77 +39,23 @@ public class ToDoItemProcessor {
                 break;
 
             case IN_PROGRESS:
-                processInProgress(toDoItem);
+                new ToDoItemInProgressState(storyService).process(toDoItem);
                 break;
 
             case DONE:
-                processDone(toDoItem);
+                new ToDoItemDoneState(eventsRegistry, storyService).process(toDoItem);
                 break;
 
             case APPROVED:
-                processApproved(toDoItem);
+                new ToDoItemApprovedState(eventsRegistry, storyService).process(toDoItem);
                 break;
 
             case RELEASED:
-                processReleased(toDoItem);
+                new ToDoItemReleasedState(eventsRegistry).process(toDoItem);
                 break;
 
             default:
                 break;
         }
-    }
-
-    private void processDefined(ToDoItem toDoItem) {
-
-    }
-
-    private void processInProgress(ToDoItem toDoItem) {
-        if (toDoItem instanceof Task) {
-            Task task = (Task) toDoItem;
-            storyService.updateProgressOf(task.getStory(), task);
-        }
-    }
-
-    private void processDone(ToDoItem toDoItem) {
-        if (toDoItem instanceof Task) {
-            Task task = (Task) toDoItem;
-            Story story = task.getStory();
-            storyService.updateProgressOf(task.getStory(), task);
-            if (DONE.equals(story.getStatus())) {
-                StoryDoneEvent event = new StoryDoneEvent();
-                event.setStoryId(story.getId());
-                eventsRegistry.publish(event);
-            }
-        } else if (toDoItem instanceof Story) {
-            Story story = (Story) toDoItem;
-            StoryDoneEvent event = new StoryDoneEvent();
-            event.setStoryId(story.getId());
-            eventsRegistry.publish(event);
-        }
-    }
-
-    private void processApproved(ToDoItem toDoItem) {
-        if (toDoItem instanceof Story) {
-            Story story = (Story) toDoItem;
-            StoryApprovedEvent event = new StoryApprovedEvent();
-            event.setStoryId(story.getId());
-            eventsRegistry.publish(event);
-        } else if (toDoItem instanceof Task) {
-            Task task = (Task) toDoItem;
-
-            if (task.isSubtask()) {
-                TaskApprovedEvent event = new TaskApprovedEvent();
-                event.setTaskId(task.getId());
-                eventsRegistry.publish(event);
-            } else {
-                storyService.attachPartialApprovalFor(task.getStory().getId(), task.getId());
-            }
-        }
-    }
-
-    private void processReleased(ToDoItem toDoItem) {
-        ToDoItemReleasedEvent event = new ToDoItemReleasedEvent();
-        event.setToDoItemId(toDoItem.getId());
-        eventsRegistry.publish(event);
     }
 }
