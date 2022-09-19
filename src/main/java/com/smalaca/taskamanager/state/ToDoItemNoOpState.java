160b7d4e0dc7ff71c122afb20b4ba5f8a1c8c634
Marcin Chrost <mchrost82@gmail.com>
2022-09-19 23:04:18 +0200

    REFACT / TASK / PATTERN - added rest of ToDoItemVisitor implementations

--- src/main/java/com/smalaca/taskamanager/state/ToDoItemNoOpState.java
+++
@@ -1,9 +0,0 @@
-package com.smalaca.taskamanager.state;
-
-import com.smalaca.taskamanager.model.interfaces.ToDoItem;
-
-public class ToDoItemNoOpState implements ToDoItemState {
-
-    @Override
-    public void process(ToDoItem toDoItem) {}
-}
--- src/main/java/com/smalaca/taskamanager/visitor/ToDoItemReleasedStateVisitor.java
+++ src/main/java/com/smalaca/taskamanager/visitor/ToDoItemReleasedStateVisitor.java
@@ -1,19 +1,18 @@
-package com.smalaca.taskamanager.state;
+package com.smalaca.taskamanager.visitor;
 
 import com.smalaca.taskamanager.events.ToDoItemReleasedEvent;
 import com.smalaca.taskamanager.model.interfaces.ToDoItem;
 import com.smalaca.taskamanager.registry.EventsRegistry;
 
-public class ToDoItemReleasedState implements ToDoItemState {
-
+public class ToDoItemReleasedStateVisitor implements ToDoItemVisitor {
     private final EventsRegistry eventsRegistry;
 
-    public ToDoItemReleasedState(EventsRegistry eventsRegistry) {
+    public ToDoItemReleasedStateVisitor(EventsRegistry eventsRegistry) {
         this.eventsRegistry = eventsRegistry;
     }
 
     @Override
-    public void process(ToDoItem toDoItem) {
+    public void visit(ToDoItem toDoItem) {
         ToDoItemReleasedEvent event = new ToDoItemReleasedEvent();
         event.setToDoItemId(toDoItem.getId());
         eventsRegistry.publish(event);
