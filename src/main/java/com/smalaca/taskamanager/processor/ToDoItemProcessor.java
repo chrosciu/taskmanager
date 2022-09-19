9dd7cba2e4d67c1f68cd560fa8c31d2ab4b6e486
Marcin Chrost <mchrost82@gmail.com>
2022-09-19 23:04:18 +0200

    REFACT / TASK / PATTERN - replaced rest of states with ToDoItemVisitorState

--- src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
+++ src/main/java/com/smalaca/taskamanager/processor/ToDoItemProcessor.java
@@ -6,36 +6,38 @@ import com.smalaca.taskamanager.service.CommunicationService;
 import com.smalaca.taskamanager.service.ProjectBacklogService;
 import com.smalaca.taskamanager.service.SprintBacklogService;
 import com.smalaca.taskamanager.service.StoryService;
-import com.smalaca.taskamanager.state.ToDoItemDefinedState;
-import com.smalaca.taskamanager.state.ToDoItemDoneState;
-import com.smalaca.taskamanager.state.ToDoItemInProgressState;
-import com.smalaca.taskamanager.state.ToDoItemNoOpState;
-import com.smalaca.taskamanager.state.ToDoItemReleasedState;
 import com.smalaca.taskamanager.state.ToDoItemState;
 import com.smalaca.taskamanager.state.ToDoItemVisitorState;
 import com.smalaca.taskamanager.visitor.ToDoItemApprovedStateVisitor;
+import com.smalaca.taskamanager.visitor.ToDoItemDefinedStateVisitor;
+import com.smalaca.taskamanager.visitor.ToDoItemDoneStateVisitor;
+import com.smalaca.taskamanager.visitor.ToDoItemInProgressStateVisitor;
+import com.smalaca.taskamanager.visitor.ToDoItemReleasedStateVisitor;
+import com.smalaca.taskamanager.visitor.ToDoItemVisitor;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ToDoItemProcessor {
-    private final ToDoItemDefinedState toDoItemDefinedState;
-    private final ToDoItemInProgressState toDoItemInProgressState;
-    private final ToDoItemDoneState toDoItemDoneState;
+    private final ToDoItemState toDoItemDefinedState;
+    private final ToDoItemState toDoItemInProgressState;
+    private final ToDoItemState toDoItemDoneState;
     private final ToDoItemState toDoItemApprovedState;
-    private final ToDoItemReleasedState toDoItemReleasedState;
-    private final ToDoItemNoOpState toDoItemNoOpState;
+    private final ToDoItemState toDoItemReleasedState;
+    private final ToDoItemState toDoItemNoOpState;
 
     public ToDoItemProcessor(
             StoryService storyService, EventsRegistry eventsRegistry, ProjectBacklogService projectBacklogService,
             CommunicationService communicationService, SprintBacklogService sprintBacklogService) {
-        toDoItemDefinedState = new ToDoItemDefinedState(projectBacklogService, communicationService,
-            sprintBacklogService,
-            eventsRegistry);
-        toDoItemInProgressState = new ToDoItemInProgressState(storyService);
-        toDoItemDoneState = new ToDoItemDoneState(eventsRegistry, storyService);
+        toDoItemDefinedState = new ToDoItemVisitorState(
+            new ToDoItemDefinedStateVisitor(
+                projectBacklogService, communicationService, sprintBacklogService, eventsRegistry
+            )
+        );
+        toDoItemInProgressState = new ToDoItemVisitorState(new ToDoItemInProgressStateVisitor(storyService));
+        toDoItemDoneState = new ToDoItemVisitorState(new ToDoItemDoneStateVisitor(eventsRegistry, storyService));
         toDoItemApprovedState = new ToDoItemVisitorState(new ToDoItemApprovedStateVisitor(eventsRegistry, storyService));
-        toDoItemReleasedState = new ToDoItemReleasedState(eventsRegistry);
-        toDoItemNoOpState = new ToDoItemNoOpState();
+        toDoItemReleasedState = new ToDoItemVisitorState(new ToDoItemReleasedStateVisitor(eventsRegistry));
+        toDoItemNoOpState = new ToDoItemVisitorState(new ToDoItemVisitor() {});
     }
 
     public void processFor(ToDoItem toDoItem) {
