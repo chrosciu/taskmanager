492f05d05e16a128e15814ad8e7af78648880109
Marcin Chrost <mchrost82@gmail.com>
2022-09-19 13:44:50 +0200

    REFACT / PATTERN - introduced ToDoItemVisitor interface and first implementation

--- src/main/java/com/smalaca/taskamanager/state/ToDoItemApprovedState.java
+++ src/main/java/com/smalaca/taskamanager/state/ToDoItemApprovedState.java
@@ -2,6 +2,7 @@ package com.smalaca.taskamanager.state;
 
 import com.smalaca.taskamanager.events.StoryApprovedEvent;
 import com.smalaca.taskamanager.events.TaskApprovedEvent;
+import com.smalaca.taskamanager.model.entities.Epic;
 import com.smalaca.taskamanager.model.entities.Story;
 import com.smalaca.taskamanager.model.entities.Task;
 import com.smalaca.taskamanager.model.interfaces.ToDoItem;
@@ -12,22 +13,46 @@ public class ToDoItemApprovedState implements ToDoItemState {
 
     private final EventsRegistry eventsRegistry;
     private final StoryService storyService;
+    private final ToDoItemApprovedStateVisitor toDoItemVisitor;
 
     public ToDoItemApprovedState(EventsRegistry eventsRegistry, StoryService storyService) {
         this.eventsRegistry = eventsRegistry;
         this.storyService = storyService;
+        toDoItemVisitor = new ToDoItemApprovedStateVisitor();
     }
 
-    @Override
-    public void process(ToDoItem toDoItem) {
-        if (toDoItem instanceof Story) {
-            Story story = (Story) toDoItem;
+    interface ToDoItemVisitor {
+        default void visit(ToDoItem toDoItem) {}
+
+        default void visit(Epic epic) {}
+
+        default void visit(Story story) {}
+
+        default void visit(Task task) {}
+
+        static void visit(ToDoItemVisitor toDoItemVisitor, ToDoItem toDoItem) {
+            if (toDoItem instanceof Task) {
+                toDoItemVisitor.visit((Task) toDoItem);
+            } else if (toDoItem instanceof Epic) {
+                toDoItemVisitor.visit((Epic) toDoItem);
+            } else if (toDoItem instanceof Story) {
+                toDoItemVisitor.visit((Story) toDoItem);
+            } else {
+                toDoItemVisitor.visit(toDoItem);
+            }
+        }
+    }
+
+    private class ToDoItemApprovedStateVisitor implements ToDoItemVisitor {
+        @Override
+        public void visit(Story story) {
             StoryApprovedEvent event = new StoryApprovedEvent();
             event.setStoryId(story.getId());
             eventsRegistry.publish(event);
-        } else if (toDoItem instanceof Task) {
-            Task task = (Task) toDoItem;
+        }
 
+        @Override
+        public void visit(Task task) {
             if (task.isSubtask()) {
                 TaskApprovedEvent event = new TaskApprovedEvent();
                 event.setTaskId(task.getId());
@@ -36,5 +61,10 @@ public class ToDoItemApprovedState implements ToDoItemState {
                 storyService.attachPartialApprovalFor(task.getStory().getId(), task.getId());
             }
         }
+    }
+
+    @Override
+    public void process(ToDoItem toDoItem) {
+        ToDoItemVisitor.visit(toDoItemVisitor, toDoItem);
     }
 }
