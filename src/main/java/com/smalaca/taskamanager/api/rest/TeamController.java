b268f054016efc74e00ac8b0bd041f56c2645975
Marcin Chrost <mchrost82@gmail.com>
2022-09-29 16:42:06 +0200

    REFACT - used Team.asTeamDto everywhere in TeamController

--- src/main/java/com/smalaca/taskamanager/api/rest/TeamController.java
+++ src/main/java/com/smalaca/taskamanager/api/rest/TeamController.java
@@ -44,20 +44,7 @@ public class TeamController {
     @GetMapping
     public ResponseEntity<List<TeamDto>> findAll() {
         List<TeamDto> teams = StreamSupport.stream(teamRepository.findAll().spliterator(), false)
-                .map(team -> {
-                    TeamDto dto = new TeamDto();
-                    dto.setId(team.getId());
-                    dto.setName(team.getName());
-
-                    if (team.hasCodename()) {
-                        dto.setCodenameShort(team.getCodename().getShortName());
-                        dto.setCodenameFull(team.getCodename().getFullName());
-                    }
-
-                    dto.setDescription(team.getDescription());
-
-                    return dto;
-                })
+                .map(Team::asTeamDto)
                 .collect(toList());
 
         return new ResponseEntity<>(teams, HttpStatus.OK);
@@ -118,17 +105,7 @@ public class TeamController {
 
         Team updated = teamRepository.save(team);
 
-        TeamDto dto = new TeamDto();
-        dto.setId(updated.getId());
-        dto.setName(updated.getName());
-        if (updated.hasCodename()) {
-            dto.setCodenameShort(updated.getCodename().getShortName());
-            dto.setCodenameFull(updated.getCodename().getFullName());
-        }
-
-        dto.setDescription(updated.getDescription());
-
-        return new ResponseEntity<>(dto, HttpStatus.OK);
+        return new ResponseEntity<>(updated.asTeamDto(), HttpStatus.OK);
     }
 
     @PutMapping("/{id}/members")
