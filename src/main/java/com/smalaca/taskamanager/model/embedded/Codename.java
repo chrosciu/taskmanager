2360faa1f8a768d36f06e9ed8d4279937ef4d606
Marcin Chrost <mchrost82@gmail.com>
2023-06-29 16:28:41 +0200

    REFACT - used Lombok to remove boilerplate code from Codename

--- src/main/java/com/smalaca/taskamanager/model/embedded/Codename.java
+++ src/main/java/com/smalaca/taskamanager/model/embedded/Codename.java
@@ -1,8 +1,12 @@
 package com.smalaca.taskamanager.model.embedded;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+@EqualsAndHashCode
 public class Codename {
     private final String shortName;
     private final String fullName;
@@ -10,45 +14,5 @@ public class Codename {
     @Deprecated
     private Codename() {
         this(null, null);
-    }
-
-    public Codename(String shortName, String fullName) {
-        this.shortName = shortName;
-        this.fullName = fullName;
-    }
-
-    public String getShortName() {
-        return shortName;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-
-        Codename codename = (Codename) o;
-
-        return new EqualsBuilder()
-                .append(shortName, codename.shortName)
-                .append(fullName, codename.fullName)
-                .isEquals();
-    }
-
-    @Override
-    @SuppressWarnings("MagicNumber")
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(shortName)
-                .append(fullName)
-                .toHashCode();
     }
 }
